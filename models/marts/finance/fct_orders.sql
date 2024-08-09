147dc17bc1e7b86e99a5e9d54f94e35c4b875754
samuel.w.faber@gmail.com <samuel.w.faber@gmail.com>
2024-08-09 15:59:16 +0000

    Update ref for fct_orders table

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -2,6 +2,6 @@ select
 ord.order_id
 ,ord.customer_id
 ,pay.amount
-from stg_jaffle_shop__orders as ord
-inner join stg_stripe__payments as pay
+from {{ ref('stg_jaffle_shop__orders') }} as ord
+inner join {{ ref('stg_stripe__payments') }} as pay
     on ord.order_id = pay.order_id
